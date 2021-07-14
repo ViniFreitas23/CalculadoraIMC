00760c5cb1a89387e5795aebf415d13f39183841
ViniFreitas23 <vinicius.freitas.correia@hotmail.com>
2021-07-14 18:13:12 -0300

    Updated IMC Calculator app

--- app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.calculadoraimc
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btCalcular : Button  = calcular
+        val resultado : TextView = resultado
+
+        btCalcular.setOnClickListener{
+            val peso: Float = (peso.text.toString()).toFloat()
+            val altura:Float = (altura.text.toString()).toFloat()
+            val imc:Float = (peso/(altura * altura))
+
+            if (imc < 18.5){
+                resultado.setText("Você está abaixo do peso!" + "\n" + "Seu IMC é: " + imc)
+                resultado.setTextColor(Color.RED)
+            } else if (imc > 24.9){
+                resultado.setText("Você está acima do peso!" + "\n" + "Seu IMC é: " + imc)
+                resultado.setTextColor(Color.RED)
+            } else {
+                resultado.setText("Seu peso está normal!" + "\n" + "Seu IMC é: " + imc)
+                resultado.setTextColor(Color.GREEN)
+            }
+
+        }
+
     }
 }
