7eb4b8fb3f928972e2c0042ba0e080baec331d17
ViniFreitas23 <vinicius.freitas.correia@hotmail.com>
2021-07-23 11:16:52 -0300

    Updated app layout, added Tabela IMC button to test second activity

--- app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +15,12 @@ class MainActivity : AppCompatActivity() {
 
         val btCalcular : Button  = calcular
         val resultado : TextView = resultado
+        val btTabela : Button = table
+
+        btTabela.setOnClickListener{
+            val intent = Intent(this, tabela::class.java)
+            startActivity(intent)
+        }
 
         btCalcular.setOnClickListener{
             val peso: Float = (peso.text.toString()).toFloat()
@@ -21,14 +28,14 @@ class MainActivity : AppCompatActivity() {
             val imc:Float = (peso/(altura * altura))
 
             if (imc < 18.5){
-                resultado.setText("Você está abaixo do peso!" + "\n" + "Seu IMC é: " + imc)
+                resultado.setText("Você está abaixo do peso!" + "\n" + "Seu IMC é: " + "%.2f".format(imc))
                 resultado.setTextColor(Color.RED)
-            } else if (imc > 24.9){
-                resultado.setText("Você está acima do peso!" + "\n" + "Seu IMC é: " + imc)
+            } else if (imc > 24.9 && imc < 30){
+                resultado.setText("Você está acima do peso!" + "\n" + "Seu IMC é: " + "%.2f".format(imc))
                 resultado.setTextColor(Color.RED)
             } else {
-                resultado.setText("Seu peso está normal!" + "\n" + "Seu IMC é: " + imc)
-                resultado.setTextColor(Color.GREEN)
+                resultado.setText("Seu peso está normal!" + "\n" + "Seu IMC é: " + "%.2f".format(imc))
+                resultado.setTextColor(Color.rgb(92, 192, 67))
             }
 
         }
